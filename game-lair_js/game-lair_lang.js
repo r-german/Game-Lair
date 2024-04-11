a81b6e036b2ea1e9b1c5d738d92e19b142fa8889
r-german <111256133+r-german@users.noreply.github.com>
2024-04-11 08:42:36 +0300

    Add files via upload

---
+++ game-lair_js/game-lair_lang.js
@@ -0,0 +1,90 @@
+const textObject = {
+    "attr-name1": {
+        "ru": "Контакты",
+        "en": "Contacts"
+    },
+    "attr-name2": {
+        "ru": "Игры",
+        "en": "Games"
+    },
+    "attr-name3": {
+        "ru": "О нас",
+        "en": "About us"
+    },
+    "attr-name4": {
+        "ru": `Мы делаем <span class="silver">игры</span> для <span class="orange">вас</span>`,
+        "en": `We create <span class="silver">games</span> for <span class="orange">you</span>`
+    },
+    "attr-name5": {
+        "ru": "Мы стремимся создавать игры, которые увлекают игроков в мир увлекательных приключений и незабываемых впечатлений. Наша команда талантливых разработчиков работает с самыми передовыми технологиями, чтобы создавать лучшие игры.",
+        "en": "We strive to create games that take players into a world of exciting adventures and unforgettable experiences. Our team of talented developers works with the most advanced technologies. Our goal is to create the best games."
+    },
+    "attr-name6": {
+        "ru": "Кто мы?",
+        "en": "Who we are"
+    },
+    "attr-name7": {
+        "ru": "Команда CorruptedEgg состоит из опытных и талантливых разработчиков, которые стремятся создавать игры, которые привлекают и завораживают игроков. Наша студия занимается разработкой игр под WebGL.",
+        "en": "The Corrupted Egg team consists of experienced and talented developers who strive to create games that attract and fascinate players. Our studio develops games for WebGL."
+    },
+    "attr-name8": {
+        "ru": "Про что мы?",
+        "en": "Ideology"
+    },
+    "attr-name9": {
+        "ru": "Наша философия заключается в создании игр, которые могут быть легко понятны и доступны для игроков всех возрастов и уровней опыта. Мы сосредоточены на том, чтобы создавать увлекательный геймплей, который заставляет игроков возвращаться к нашим играм снова и снова.",
+        "en": "Our philosophy is to create games that can be easily understood and accessible to players of all ages and experience levels. We are focused on creating exciting gameplay that keeps players coming back to our games over and over again."
+    },
+    "attr-name10": {
+        "ru": "Наши игры",
+        "en": "Our games"
+    },
+    "attr-name11": {
+        "ru": "Играть",
+        "en": "Play"
+    },
+    "attr-name12": {
+        "ru": "Играть",
+        "en": "Play"
+    },
+    "attr-name13": {
+        "ru": "Играть",
+        "en": "Play"
+    },
+    "attr-name14": {
+        "ru": "Играть",
+        "en": "Play"
+    },
+    "attr-name15": {
+        "ru": "Играть",
+        "en": "Play"
+    },
+    "attr-name16": {
+        "ru": "Играть",
+        "en": "Play"
+    },
+    "attr-name17": {
+        "ru": "О нас",
+        "en": "About us"
+    },
+    "attr-name18": {
+        "ru": "Специализация студии CorruptedEgg - это гиперказуальные игры. Мы уделяем особое внимание разработке игр, которые привлекают игроков своей простотой и доступностью, но при этом предлагают уникальный и интересный геймплей. Мы стремимся создавать игры, которые увлекают игроков в мир увлекательных приключений и незабываемых впечатлений. Наша команда талантливых разработчиков работает с самыми передовыми технологиями, чтобы создавать лучшие игры. Мы гордимся своим опытом и достижениями, и всегда стремимся к новым высотам в разработке игр. Мы всегда ищем новых талантливых людей, которые хотят присоединиться к нам и создавать лучшие игры в индустрии. Спасибо, что заинтересовались нашей компанией и нашей студией CorruptedEgg. Мы надеемся, что вы насладитесь играми, которые мы создаем, и с нетерпением ждем будущих проектов и новых достижений в мире видеоигр.",
+        "en": "Corrupted Egg Studio specializes in hyper casual games. We pay special attention to the development of games that attract players with their simplicity and accessibility, but at the same time offer a unique and interesting gameplay. We strive to create games that take players into a world of exciting adventures and unforgettable experiences. Our team of talented developers works with the most advanced technologies to create the best games. We are proud of our experience and achievements, and always strive for new heights in game development. We are always looking for new talented people who want to join us and create the best games in the industry. Thank you for being interested in our company and our Corrupted Egg studio. We hope that you will enjoy the games we create and look forward to future projects and new achievements in the world of video games."
+    },
+    "attr-name19": {
+        "ru": "Игры",
+        "en": "Games"
+    },
+    "attr-name20": {
+        "ru": "Наша команда",
+        "en": "Our team"
+    },
+    "attr-name21": {
+        "ru": "К началу страницы",
+        "en": "Scroll to the top"
+    },
+    "attr-name22": {
+        "ru": "Контакты",
+        "en": "Contacts"
+    }
+};
